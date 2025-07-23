2ee2f8e75041abf7371d422597dcecc71c1db8ee
Jinka <jinka97@gmail.com>
2025-07-24 00:16:28 +0100

    refactor: use RouterProvider to pass router to app

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './router';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from './QueryClient';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
+    <QueryClientProvider client={queryclient}>
+      <RouterProvider router={router} />
     </QueryClientProvider>
   </React.StrictMode>
 );
